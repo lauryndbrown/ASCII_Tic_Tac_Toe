95d81141b597b20f57ed1bd6f40174640a9a9a63
lauryndbrown <lauryndbrown@gmail.com>
2017-02-13 11:57:41 -0600

    Set up the in game menus.

--- tic_tac_toe/tic_tac_toe_display.py
+++ tic_tac_toe/tic_tac_toe_display.py
@@ -2,14 +2,34 @@ from ascii_game.game_display.display import Display
 from ascii_game.game_display.input_tools import *
 
 class TicTacToeDisplay(Display):
+    TITLE_OFFSET = 3
+    IN_GAME_MENU_OFFSET = 4
+    GAME_SCREEN_OFFSET = 1 + TITLE_OFFSET + IN_GAME_MENU_OFFSET 
+    SETTINGS_SCREEN_OFFSET = 0 + TITLE_OFFSET + IN_GAME_MENU_OFFSET
     def __init__(self):
         col_size = 50
         super().__init__(col_size)
     def start_menu(self):
-        print("TicTacToe")
+        #self.clear_screen()
+        #image = Image.open(self.IMAGES+"title.png")
+        #ascii_img = self.image_converter.image_to_ascii(image, 300)
+        #print(self.center("Tic Tac Toe", self.HR_BOLD))
+        #print(ascii_img, end="")
+        #print(self.format_HR(' ')) 
+        pass
     def game_screen(self, game):
+        self.clear_screen()
+        print(self.center("Tic Tac Toe", self.HR_BOLD))
         print("game_screen")
+        self.fill_screen(self.GAME_SCREEN_OFFSET)
+        self._in_game_menu(game.menu)
     def settings_screen(self, game):
-        print("settings")
+        self.clear_screen()
+        print(self.center("Settings", self.HR_BOLD))
+        self.fill_screen(self.SETTINGS_SCREEN_OFFSET)
+        self._in_game_menu(game.menu)
     def exit_screen(self):
-        print("exit")
+        print(self.center("Thanks for Playing!", ' '))
+    def clear_screen(self):
+        lines = self.get_terminal_lines()
+        print(lines*"\n")
