95669622f039c0994b7a4d13319e4b0d9477aa4f
lauryndbrown <lauryndbrown@gmail.com>
2017-02-13 17:34:38 -0600

    Added start menu. Start menu plays first.

--- tic_tac_toe/tic_tac_toe_display.py
+++ tic_tac_toe/tic_tac_toe_display.py
@@ -1,5 +1,6 @@
 from ascii_game.game_display.display import Display
 from ascii_game.game_display.input_tools import *
+from tic_tac_toe.game_entities import TicTacToe
 from PIL import Image
 
 class TicTacToeDisplay(Display):
@@ -13,10 +14,14 @@ class TicTacToeDisplay(Display):
     def __init__(self):
         col_size = 50
         super().__init__(col_size)
-    def start_menu(self, game):
-        self.clear_screen()
+        self._create_ascii_images()
+        self._create_board_parts()
+    def _create_ascii_images(self):
+        """
+        Used in __init__ 
+        Create Images and Convert Images to ascii
+        """
         title_image = Image.open(self.IMAGES+"title.png")
-        in_game_title_image = Image.open(self.IMAGES+"title.png")
         x_image = Image.open(self.IMAGES+"x.png")
         o_image = Image.open(self.IMAGES+"o.png")
         line_image = Image.open(self.IMAGES+"line.png")
@@ -24,26 +29,31 @@ class TicTacToeDisplay(Display):
         self.image_converter.row_incr = 5
         self.image_converter.col_incr = 5
         title_image = self.image_converter.scale_image(title_image, 400)
-        in_game_title_image = self.image_converter.scale_image(in_game_title_image, 300)
         o_image = o_image.resize((50,25))
         x_image = x_image.resize((50,25))
         self.ascii_o = self.image_converter.image_to_ascii(o_image)
         self.ascii_x = self.image_converter.image_to_ascii(x_image)
         self.image_converter.invert_chars()
         self.ascii_title = self.image_converter.image_to_ascii(title_image)
-        self.ascii_in_game_title = self.image_converter.image_to_ascii(in_game_title_image)
-
+    def _create_board_parts(self):
+        """
+        Used in __init__
+        Create Board images and Dictionary Mapping
+        """
         self.ascii_empty = ((" "*10)+"\n")*5
         self.ascii_vline = "  |  \n  |  \n  |  \n  |  \n  |  \n"
         self.ascii_line ="-"*40 
-        
         self.board = {
-                    game.game_board.EMPTY:self.ascii_empty,
-                    game.game_board.O:self.ascii_o,
-                    game.game_board.X:self.ascii_x
+                    TicTacToe.EMPTY:self.ascii_empty,
+                    TicTacToe.O:self.ascii_o,
+                    TicTacToe.X:self.ascii_x
                         }    
+        
+    def start_menu(self, game):
+        self.clear_screen()
         print(self.ascii_title)
-        #print(self.ascii_in_game_title)
+        self._in_game_menu(game.menu)
+        self.last_menu = (self.game_screen, (game,))
     def build_board(self, board):
         def build_row(values):
             col1 = self.board[values[0]] 
--- tic_tac_toe/tic_tac_toe_game.py
+++ tic_tac_toe/tic_tac_toe_game.py
@@ -5,6 +5,7 @@ from tic_tac_toe.tic_tac_toe_display import TicTacToeDisplay
 
 class TicTacToeGame(Game):
     #Menu Names
+    START_MENU_NAME = "Start"
     GAME_MENU_NAME = "Game"
     SETTINGS_MENU_NAME = "Settings"
     #Option Names
@@ -12,18 +13,22 @@ class TicTacToeGame(Game):
     def __init__(self, display, player1, player2):
         super().__init__(display, player1, player2)
         self.game_board = TicTacToe()
+        start_menu = []
         game_menu = []
         settings_menu = []
+        #Start Menu
+        start_menu.append(Choice("Start Game",self.display.game_screen, (self,), self.GAME_MENU_NAME))
+        start_menu.append(Choice("Exit TicTacToe",self.end_game, None, None))
         #Game Menu
-        game_menu.append(Choice("End Game",self.end_game, None, None))
+        game_menu.append(Choice("End Game",self.display.start_menu, (self,), self.START_MENU_NAME))
         game_menu.append(Choice("Settings",self.display.settings_screen, (self,), self.SETTINGS_MENU_NAME))
         game_menu.append(Choice("Move", self.move, (), None))
         #Settings Menu
         settings_menu.append(Choice(self.BACK_OPTION,self.display.game_screen, (self,), self.GAME_MENU_NAME))
         #settings_menu.append(Choice("Toggle 1 or 2 Players",self.display.game_screen, (self,), self.GAME_MENU))
-        self.menus = {self.GAME_MENU_NAME:game_menu, self.SETTINGS_MENU_NAME:settings_menu}
-        #Current game menu is pointed to by self.menu
-        self.menu = game_menu
+        self.menus = {self.START_MENU_NAME:start_menu, self.GAME_MENU_NAME:game_menu, self.SETTINGS_MENU_NAME:settings_menu}
+        #Current menu is pointed to by self.menu
+        self.menu = start_menu
         #Because the game has just started the previous menu is None
         self.prev_menu = None
         self.player_letter = self.game_board.X
